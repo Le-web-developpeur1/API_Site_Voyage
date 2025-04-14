f8f7da9465e3ed730b93ff11150759537f004783
Lemagiciendelombre <booba8033@gmail.com>
2025-04-14 12:22:25 +0000

    API_Site_Voyage

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get("/", (req, res) => {
     res.send("Bienvenue sur ExplorePlus API");
 });
 
-const PORT = 5000;
+const PORT =process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Serveur lancé sur le port ${PORT}`);
